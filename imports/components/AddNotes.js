f4707fb84c472e1c9282e8d9328338566eb5b881
Dorian Taing <doriantaing77@hotmail.fr>
2019-01-14 14:08:15 +0100

    add select

--- imports/components/AddNotes.js
+++ imports/components/AddNotes.js
@@ -1,14 +1,26 @@
 import React from "react";
+import { withTracker } from "meteor/react-meteor-data";
+import { Students } from "../api/Student";
 
-const AddNotes = () => {
+
+const AddNotes = (props) => {
   return (
     <div>
       <form action="">
         <input type="text" />
         <input type="number" />
+        <select name="" id="">
+        {props.students.map(student => {
+          return(
+            <option value={student.data.name} key={student._id}>{student.data.firstname} {student.data.name}</option>
+          )
+        })}
+        </select>
       </form>
     </div>
   );
 };
 
-export default AddNotes;
+export default withTracker(() => {
+  return { students: Students.find({}).fetch() };
+})(AddNotes);
