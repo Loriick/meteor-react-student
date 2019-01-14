6d9ef90adf71ff6b4efd27dcef90267199ded47d
Travailleur Lorick <loricktravailleur@gmail.com>
2019-01-14 15:57:25 +0100

    student

--- imports/api/Student.js
+++ imports/api/Student.js
@@ -1,19 +1,4 @@
 import { Mongo } from "meteor/mongo";
 
-export const Notes = new Mongo.Collection("notes", {
-  transform: function(doc) {
-    doc.notesObj = Students.find({
-      _id: doc.students
-    });
-    return doc;
-  }
-});
-
-export const Students = new Mongo.Collection("students", {
-  transform: function(doc) {
-    doc.studentsObj = Notes.find({
-      students: doc._id
-    });
-    return doc;
-  }
-});
+export const Students = new Mongo.Collection("students");
+export const Notes = new Mongo.Collection("notes");
